2cdb75df5e9bd485b853978fd0838a0c52810769
optimizasean <optimizasean@gmail.com>
2018-04-13 00:53:29 -1000

    SQL connection completion
    Basic connection works, just use the query method to query things with anything you want.

--- DrinkDB/src/drinkdb/DatabaseConnector.java
+++ DrinkDB/src/drinkdb/DatabaseConnector.java
@@ -7,33 +7,67 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.DriverManager;
 
+/**
+ *
+ * @author Lorraine Jimenez, Sean Harding
+ */
 public class DatabaseConnector {
-    String hostName = "drinkmap.database.windows.net";
-    String port = "1433";
-    String dbName = "DrinkMap";
-    String user = "Application";
-    String password = "s5#d(V)2`c Ldj2#9!~";
-    String encrypt = "true";
-    String loginTimeout = "30";
-    String url = String.format("jdbc:sqlserver://%s:%s;database=%s;user=%s;password=%s;encrypt=%s;hostNameInCertificate=*.database.windows.net;loginTimeout=%s;",
-        hostName, port, dbName, user, password, encrypt, loginTimeout);
-    Connection connection = null;
+    private String hostName = "drinkmap.database.windows.net";
+    private String port = "1433";
+    private String dbName = "DrinkMap";
+    private String user = "Application";
+    private String password = "s5#d(V)2`c Ldj2#9!~";
+    private String encrypt = "true";
+    private String hostNameInCertificate = "*.database.windows.net";
+    private String loginTimeout = "30";
+    private String url = String.format("jdbc:sqlserver://%s:%s;database=%s;user=%s;password=%s;encrypt=%s;hostNameInCertificate=%s;loginTimeout=%s;",
+        hostName, port, dbName, user, password, encrypt, hostNameInCertificate, loginTimeout);
+    private Connection conn = null;
+    //Statement statement = null; << Attempt to globalize
 
     //Default Constructor
     public DatabaseConnector() {
         try {
-            connection = DriverManager.getConnection(url);
-            String schema = connection.getSchema();
+            conn = DriverManager.getConnection(url);
+            String schema = conn.getSchema();
+            System.out.println("Successful connection - Schema: " + schema);
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+            System.err.println("CONNECTION FAILED");
+        } finally {
+            System.err.println("Attempting 2nd connect...");
+            System.out.println((connect()) ? "Connected" : "2nd CONNECT FAIL");
+        }
+    }
+
+    public boolean connect() {
+        try {
+            conn = DriverManager.getConnection(url);
+            String schema = conn.getSchema();
             System.out.println("Successful connection - Schema: " + schema);
         } catch (SQLException sqle) {
             sqle.printStackTrace();
             System.err.println("CONNECTION FAILED");
         }
+        return (conn == null) ? false : true;
+    }
+    public disconnect() {
+        //statement = null; << for globalize
+        conn.close();
+        conn = null;
     }
 
-    //SQL Query method
-    public ResultSet databaseQuery() {
+    //SQL Query method for SQL testing
+    public ResultSet databaseQuery(String query) {
         //Finish this, figure out other ones...
-        return null;
+        ResultSet result = null;
+        try {
+            Statement statement = connection.createStatement();
+            result = statement.executeQuery(query);
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+            System.err.println("SQL STATEMENT FAILED");
+        }
+        return result;
     }
 }
