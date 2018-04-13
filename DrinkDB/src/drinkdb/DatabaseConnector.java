ea45eb65c10abec86e5b3d8781e29422a3fc74a2
optimizasean <optimizasean@gmail.com>
2018-04-12 17:10:51 -1000

    cHANGES
    no work yet

--- DrinkDB/src/drinkdb/DatabaseConnector.java
+++ DrinkDB/src/drinkdb/DatabaseConnector.java
@@ -4,20 +4,34 @@ import java.sql.Connection;
 import java.sql.Statement;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.DriverManager;
 
 public class DatabaseConnector {
     String hostName = "drinkmap.database.windows.net";
-    String dbname = "DrinkMap";
+    String port = "1433";
+    String dbName = "DrinkMap";
     String user = "Application";
     String password = "s5#d(V)2`c Ldj2#9!~";
-    String url = String.format("jdbc:sqlserver://%s:1433;database=%s;user=%s;password=%s;encrypt=true;hostNameInCertificate=*.database.windows.net;loginTimeout=30;", hostName, dbName, user, password);
+    String encrypt = "true";
+    String loginTimeout = "30";
+    String url = String.format("jdbc:sqlserver://%s:%s;database=%s;user=%s;password=%s;encrypt=%s;hostNameInCertificate=*.database.windows.net;loginTimeout=%s;",
+        hostName, port, dbName, user, password, encrypt, loginTimeout);
     Connection connection = null;
 
     //Default Constructor
     public DatabaseConnector() {
-
+        try {
+            connection = DriverManager.getConnection(url);
+            String schema = connection.getSchema();
+            System.out.println("Successful connection - Schema: " + schema);
+        } catch (SQLException sqle) {
+            System.err.println("CONNECTION FAILED");
+        }
     }
 
-
+    //SQL Query method
+    public ResultSet databaseQuery() {
+        return null;
+    }
 }
